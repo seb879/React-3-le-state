b79f2607a48e44954af267f7769abf55955b1046
Sébastien Jolly <seb_879@hotmail.fr>
2019-03-28 10:11:05 +0100

    react 3 state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Quotes from "./Quotes";
+import Lamp from "./Lamp";
 
 class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: props.working
+    };
+  }
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+  }
+
+
   render() {
+    const pause = this.state.working ? 'working' : 'onBreak';
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Simpsons Quotes</h1>
+          <img src={logo} className={`App-logo ${pause}`} alt="logo" />
+          {/* <h1 className="App-title">Simpsons Quotes</h1> */}
         </header>
+        <button
+          className={pause}
+          onClick={
+            this.handleClick
+          }>
+          {pause.toUpperCase()}
+        </button>
+        <Quotes />
+        <div className="Lamp">
+
+        </div>
       </div>
     );
   }
